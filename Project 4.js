c77caff63ee1d35f6fef6e1b91c3a3701b7aca70
JoshuaMonty <24510591+JoshuaMonty@users.noreply.github.com>
2018-04-11 13:49:22 -0400

    Update Project 4.js

--- Project 4.js
+++ Project 4.js
@@ -2,24 +2,20 @@
 const PROMPT = require('readline-sync');
 
 let continueResponse;
-let currentMonth, currentGrade, currentClassroom, upperTuition;
-const MAX_GRADE = 8,
-    MAX_MONTH = 9,
-    MAX_CLASSROOM = 3,
-    KDG_TUITION = 80;
+let currentRating;
+const MAX_RATING = 5;
 
 function main() {
     process.stdout.write('\x1Bc');
     setContinueResponse();
     while (continueResponse === 1) {
-        setCurrentMonth();
-        setCurrentGrade();
-        setCurrentClassroom();
-        processPaymentCoupons();
+        setCurrentRating();
         setContinueResponse();
-        for (let i = 0; i < MAX_CLASSROOM; i++) {
-            printGoodbye();
+        for (i = 0; i < numbers.length; i += 1) {
+            total += numbers[i];
         }
+        return total / numbers.length;
+        printGoodbye();
     }
 }
 
@@ -36,52 +32,21 @@ function setContinueResponse() {
     }
 }
 
-function setCurrentMonth() {
-    if (currentMonth != null && currentMonth <= MAX_MONTH) {
-        currentMonth++;
+function setCurrentRating() {
+    if (currentRating != null && currentRating <= MAX_RATING) {
+        currentRating++;
     } else {
-        currentMonth = 1;
+        currentRating = 1;
     }
-}
-
-function setCurrentGrade() {
-    if (typeof currentGrade !=='undefined' && currentGrade <= MAX_GRADE) {
-        currentGrade++;
-    } else {
-        currentGrade = 0;
-    }
-}
-
-function setCurrentClassroom() {
-    if (typeof currentClassroom !=='undefined' && currentClassroom <= MAX_CLASSROOM) {
-        currentClassroom++;
-    } else {
-        currentClassroom = 1;
-    }
-}
-
-function setUpperTuition() {
-    const BASE_TUITION = 60;
-    upperTuition = BASE_TUITION * currentGrade;
 }
 
 function processPaymentCoupons() {
-    while (currentGrade <= MAX_GRADE) {
-        while (currentClassroom <= MAX_CLASSROOM) {
-            while (currentMonth <= MAX_MONTH) {
-                if (currentGrade == 0) {
-                    console.log(`\n\tThe tuition for month: ${currentMonth}, for classroom: ${currentClassroom}, of grade: ${currentGrade} is: \$${KDG_TUITION}.`);
-                } else {
-                    setUpperTuition();
-                    console.log(`\n\tThe tuition for month: ${currentMonth}, for classroom: ${currentClassroom}, of grade: ${currentGrade} is: \$${upperTuition}.`);
-                }
-                setCurrentMonth();
-            }
-            setCurrentClassroom();
-            setCurrentMonth();
+    while (currentRating <= MAX_RATING) {
+        if (currentGrade == 0) {
+            console.log(`\n\tThe average rating for this movie is: ${currentRating}`);
+            } else {
+            setCurrentRating();
         }
-        setCurrentGrade();
-        setCurrentClassroom();
     }
 }
 
