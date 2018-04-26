15186f6652564f7189174745027e31a83253f154
JoshuaMonty <24510591+JoshuaMonty@users.noreply.github.com>
2018-04-26 12:38:52 -0400

    Update Project 4.js

--- Project 4.js
+++ Project 4.js
@@ -1,56 +1,94 @@
 "use strict";
 const PROMPT = require('readline-sync');
+const MOVIE_ID_REGEX = /^[0-9]*$/;
+const MOVIE_RATING_REGEX = /^[0-5]$/;
 
-let continueResponse;
-let currentRating;
-let processRatings;
-const MAX_RATING = 5;
+let movies;
+let sortOrder;
 
 function main() {
-    process.stdout.write('\x1Bc');
-    setContinueResponse();
-    while (continueResponse === 1) {
-        setCurrentRating();
-        setContinueResponse();
-        for (i = 0; i < numbers.length; i += 1) {
-            total += numbers[i];
-        }
-        return total / numbers.length;
-        printGoodbye();
-    }
+	movies = [];
+	setSortOrder();
+	while (true) {
+		calculateMovieAverage();
+        showMovies();
+		sortMovies();
+		showMainMenu();
+        addNewMovie();
+        addMovieRating();
+		askForMenuOption();
+		console.log("\n\n");
+	}
 }
 
 main();
 
-function setContinueResponse() {
-    if (continueResponse != null) {
-        continueResponse = -1;
-        while (continueResponse !== 0 && continueResponse !== 1) {
-            continueResponse = Number(PROMPT.question(`\nDo you want to continue? [0=no, 1=yes]: `));
-        }
-    } else {
-        continueResponse = 1;
-    }
+function calculateMovieAverage() {
+	for (let i = 0; i < movies.length; i++) {
+		let sumOfRatings = movies[i].ratings.reduce((a, b) => a + b, 0);
+		movies[i].average = sumOfRatings / movies[i].ratings.length;
+	}
 }
 
-function setCurrentRating() {
-    if (currentRating != null && currentRating <= MAX_RATING) {
-        currentRating++;
-    } else {
-        currentRating = 1;
-    }
+function showMovies() {
+	console.log(SPACER);
+	for (let i = 0; i < movies.length; i++) {
+		console.log("\t" + String(i + 1) + ". " + movies[i].title + " (" + movies[i].average + " stars)");
+	}
 }
 
-function processRatings() {
-    while (currentRating <= MAX_RATING) {
-        if (currentGrade == 0) {
-            console.log(`\n\tThe average rating for this movie is: ${currentRating}`);
-            } else {
-            setCurrentRating();
-        }
-    }
+function sortMovies() {
+	movies.sort((a, b) => {
+		if (sortOrder == "high to low") {
+			return a.average < b.average; // High to low
+		} else {
+			return a.average > b.average; // Low to high
+		}
+	});
 }
 
-function printGoodbye() {
-    console.log(`\tGoodbye.`);
+function showMainMenu() {
+	console.log("Sort: " + sortOrder);
+	console.log("Select a movie, type 'new' to add a new movie,");
+	console.log("or 'sort' to change sort order.");
+}
+
+function addNewMovie() {
+	console.log("\n");
+	let title = PROMPT.question("Movie title: ");
+	movies.push({
+		title: title,
+		ratings: []
+	});
+}
+
+function addMovieRating(id) {
+	let rating = PROMPT.questionInt("Your rating: ");
+	if (!MOVIE_RATING_REGEX.test(rating)) {
+		console.log("--- Movie rating invalid - Try again ---");
+		addMovieRating(id);
+	} else {
+		movies[id].ratings.push(rating);
+	}
+}
+
+function askForMenuOption() {
+	let selection = PROMPT.question(" >> ");
+	switch (selection) {
+		case "new":
+			addNewMovie();
+			addMovieRating(movies.length - 1);
+			break;
+		case "sort":
+			setSortOrder();
+			break;
+		default:
+			if (selection == "" || (!MOVIE_ID_REGEX.test(selection) || selection > movies.length)) {
+				console.log("--- Movie ID invalid - Try again ---");
+				askForMenuOption();
+			}
+			addMovieRating(Number(selection) - 1);
+			break;
+
+	}
 }
